e90fe434f2b132dc695690b77686cd04cd38a406
JUAN FELIPE BARAHONA GONZALEZ <jbarahona@xm.com.co>
2023-06-15 23:28:14 -0500

    inputs for header component were added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @Input() totalIncomes = 0;
+  @Input() totalExpenses = 0;
 
+  getTotalBudget() {
+    return this.totalIncomes - this.totalExpenses;
+  }
+
+  getExpensesPercentage() {
+    const percentage = (this.totalExpenses * 100) / this.totalIncomes;
+    if (isNaN(percentage)) {
+      return 0;
+    }
+    return percentage;
+  }
 }
