ff84b7938c9e207db8116cccddac96d4433848a1
JUAN FELIPE BARAHONA GONZALEZ <jbarahona@xm.com.co>
2023-06-15 22:57:21 -0500

    new components were added and default html was splitted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
