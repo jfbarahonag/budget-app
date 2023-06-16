dbdc352ddd9d64cdb3ef818467a5e605de4e8d9d
JUAN FELIPE BARAHONA GONZALEZ <jbarahona@xm.com.co>
2023-06-15 23:39:21 -0500

    methods to get total values were added to header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   constructor(private budgetService: BudgetService) {}
 
+  getTotalIncomes() {
+    return this.budgetService.getIncomes().reduce((acc, item) =>
+      item.value + acc, 0
+    )
+  }
+
+  getTotalExpenses() {
+    return this.budgetService.getExpenses().reduce((acc, item) =>
+      item.value + acc, 0
+    )
+  }
+
   title = 'budget';
 }
